30365cbab97c489630e9c5c294dfb66f6a15c6e5
chenc <double_cl@163.com>
2020-02-10 21:38:17 +0800

    feat: init commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/api/v1/demo')
+  getHello(): any {
     return this.appService.getHello();
+  }
+
+  @Post('/api/v1/login')
+  login(@Body() params: any): Observable<string> {
+    return params;
   }
 }
